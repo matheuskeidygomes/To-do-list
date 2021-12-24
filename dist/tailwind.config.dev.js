cdae1a49546779aa97ebbdc88e19c799d32a72a4
Matheus Keidy <matheuskeidygomes@gmail.com>
2021-12-24 16:51:37 -0300

    Uploading

---
+++ dist/tailwind.config.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class',
+  // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
